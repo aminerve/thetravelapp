be5158ceadf7c3c134c80bf8e7b41c63f350e525
aminerve <122647915+aminerve@users.noreply.github.com>
2023-06-22 10:43:09 -0400

    single country styling complete

--- src/pages/SingleCountry.js
+++ src/pages/SingleCountry.js
@@ -36,20 +36,23 @@ export default function SingleCountry() {
         </div>
       )} */}
       {country.map((country,index)=> (
-                <div key={index}>
+                <div key={index} className='grid grid-cols1 gap-8 p-8 md:grid-cols-2 bg-slate-500 rounded hover:bg-slate-400 transition-all duration-200'>
                 <article>
-                  <img src={country.flags.png} alt={country.common?.name} />
+                  <img src={country.flags.png} alt={country.name.common} />
                 </article>
                 <article>
-                  <h1>{country.common?.name}</h1>
-                  <p>Capital: {country.capital}</p>
-                  <p>Region: {country.region}</p>
-                  <p>Population: {new Intl.NumberFormat().format(country.population)}</p>
+                  <h1 className='text-3xl font-bold text-blue-900 mb-8 lg:text-5xl'>{country.name.common}</h1>
+                  <ul className='text-blue-900 leading-loose lg:leading-relaxed'>
+                    <li><span className='font-bold text-blue-900'>Capital: </span>{country.capital}</li>
+                    <li><span className='font-bold text-blue-900'>Region: </span>{country.region}</li>
+                    <li><span className='font-bold text-blue-900'>Population: </span>{new Intl.NumberFormat().format(country.population)}</li>
+                    </ul>
+                      <Link to='/' className='inline-block bg-blue-900 py-4 px-12 rounded mt-8 text-white hover:bg-blue-800 transition-all duration-200'>&larr; Back</Link>
                 </article>
               </div>
       ))}
 
-      <Link to='/' className='inline-block bg-slate-600 py-4 px-12 rounded mt-8 text-white hover:bg-slate-400 transition-all duration-200'>&larr; Back</Link>
+
     </section>
   );
 }
